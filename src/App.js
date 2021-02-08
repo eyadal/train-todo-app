c09bc7014a09bed0c0540b84587975dd2073e39a
eyadal <31968847+eyadal@users.noreply.github.com>
2021-02-09 00:44:48 +0100

    restructer the files, created elements, useEffect, eventhandlers,  Functions, ul with map method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+
+
 
 function App() {
+  const [inputVal, setInputVal] = useState('')
+  const [todos, setTodos] = useState([''])
+
+  const handleChange = (e)=> {
+    setInputVal(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTodos([...todos, inputVal])
+    e.target[0].value =''
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+      {/** 
+       * 2. event handler for the submit ( )  */}
+        <input onChange={handleChange} placeholder='Add Todo..' />
+        <button>Add Todo</button>
+    </form> 
+      <ul>
+        {todos.map( (todo, index) => (
+          todo !== '' ?
+          <li key={index}>{todo}</li> : null
+        ))}
+      </ul>
+    
+    </>
   );
 }
 
